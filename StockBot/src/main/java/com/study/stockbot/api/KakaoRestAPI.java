62b53ee17e40a0d46ebcdf4c8462a4fc3a12e20b
Lee ChaeHyeon <chlee1001@naver.com>
2021-08-01 22:39:12 +0900

    21.08.01_ConnectionTest2

--- StockBot/src/main/java/com/study/stockbot/api/KakaoRestAPI.java
+++ StockBot/src/main/java/com/study/stockbot/api/KakaoRestAPI.java
@@ -12,36 +12,36 @@ import java.util.Map;
 public class KakaoRestAPI {
 
     //카카오톡 오픈빌더로 리턴할 스킬 API
-//    @RequestMapping(value = "/kkoChat", method = {RequestMethod.POST, RequestMethod.GET}, headers = {"Accept=application/json"})
-//    public String callAPI(@RequestBody Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
-//
-//        try {
-//            ObjectMapper mapper = new ObjectMapper();
-//            String jsonInString = mapper.writeValueAsString(params);
-//            System.out.println(jsonInString);
-//            int x = 0;
-//        } catch (Exception e) {
-//
-//        }
-//        return "index";
-//    }
-    // 키보드
-    @RequestMapping(value = "/keyboard", method = RequestMethod.GET)
-    public String keyboard() {
+    @RequestMapping(value = "/kkoChat", method = {RequestMethod.POST, RequestMethod.GET}, headers = {"Accept=application/json"})
+    public String callAPI(@RequestBody Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
 
-        System.out.println("/keyboard");
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String jsonInString = mapper.writeValueAsString(params);
+            System.out.println(jsonInString);
+            int x = 0;
+        } catch (Exception e) {
 
-        JSONObject jobjBtn = new JSONObject();
-        /*JSONArray jarray = new JSONArray();
-        jarray.add("선택1");
-        jarray.add("선택2");
-        jarray.add("선택3");*/
-
-        jobjBtn.put("type", "text");
-        //jobjBtn.put("buttons", jarray);
-
-        return jobjBtn.toJSONString();
+        }
+        return "index";
     }
+//    // 키보드
+//    @RequestMapping(value = "/keyboard", method = RequestMethod.GET)
+//    public String keyboard() {
+//
+//        System.out.println("/keyboard");
+//
+//        JSONObject jobjBtn = new JSONObject();
+//        /*JSONArray jarray = new JSONArray();
+//        jarray.add("선택1");
+//        jarray.add("선택2");
+//        jarray.add("선택3");*/
+//
+//        jobjBtn.put("type", "text");
+//        //jobjBtn.put("buttons", jarray);
+//
+//        return jobjBtn.toJSONString();
+//    }
 
 
 }
