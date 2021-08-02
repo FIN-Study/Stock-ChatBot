c8e6b6dafdd16b2e2f542a755fd3af087a2cc924
yujinn <heather0220@naver.com>
2021-08-02 12:54:30 +0900

    spring mysql 연동

---
+++ StockBot/src/main/java/com/study/stockbot/repo/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.study.stockbot.repo;
+
+import com.study.stockbot.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<MemberEntity,Long> {
+}
