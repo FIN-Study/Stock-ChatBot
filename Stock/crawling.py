5a46c79136808fe15eb3c301d073a5b31ae8636a
Lee ChaeHyeon <chlee1001@naver.com>
2021-08-10 12:29:46 +0900

    21.08.10_Code_variable reset

--- Stock/crawling.py
+++ Stock/crawling.py
@@ -1,57 +1,62 @@
-# DB 연결
-import pymysql
-conn = pymysql.connect(
-  # private key
-)
-
-curs = conn.cursor()
-
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 from datetime import datetime
 
+# DB 연결
+import pymysql
+
+conn = pymysql.connect(
+    # private key
+)
+
+curs = conn.cursor()
+
+
 def get_price(company_code):
-    url = 'https://finance.naver.com/item/main.nhn?code=' + stock_to_code[key]
+    url = 'https://finance.naver.com/item/main.nhn?code=' + company_code
     source_code = requests.get(url)
     bs_obj = BeautifulSoup(source_code.content, "html.parser")
-    no_today = bs_obj.find("p", {"class" : "no_today"})
+    no_today = bs_obj.find("p", {"class": "no_today"})
     blind = no_today.find("span", {"class": "blind"})
     now_price = blind.text
     return now_price
 
+
 def get_rate(company_code):
-    url = 'https://finance.naver.com/item/sise.nhn?code=' + stock_to_code[key]
+    url = 'https://finance.naver.com/item/sise.nhn?code=' + company_code
     source_code = requests.get(url)
     bs_obj = BeautifulSoup(source_code.content, "html.parser")
     no_exday = bs_obj.select_one('#_rate > span')
     now_rate = no_exday.text
     return now_rate.strip()
 
-dfstockcode = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download',
-                           header=0)[0]
-print(dfstockcode)
-print(dfstockcode.info())
-print(dfstockcode.isnull().sum())
 
-dfstockcode.종목코드 = dfstockcode.종목코드.map('{:06d}'.format)
-print(dfstockcode)
+df_stock = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download',
+                        header=0)[0]
+# print(df_stock)
+# print(df_stock.info())
+# print(df_stock.isnull().sum())
 
-stock_to_code = dict(zip(dfstockcode.회사명, dfstockcode.종목코드))
-print(stock_to_code)
+df_stock.종목코드 = df_stock.종목코드.map('{:06d}'.format)
+# print(df_stock)
 
-df = pd.DataFrame(columns=['종목코드', '종목명', '시간', '가격', '등락률'])
-for key in stock_to_code:
+stock_to_code = tuple(zip(df_stock.회사명, df_stock.종목코드))
+# print(stock_to_code)
+
+df = pd.DataFrame(columns=['stockcode', 'stockname', 'time', 'price', 'rate'])
+for stock in stock_to_code:
+    # ('DRB동일', '004840'), ('DSR', '155660'),...stock[0] = stockName, stock[1] = stockCode
     now = datetime.now()
-    #print(now)
+    # print(now)
 
-    now_price = get_price(stock_to_code[key])
-    now_rate = get_rate(stock_to_code[key])
-    df = df.append(pd.DataFrame([[stock_to_code[key], key, now, now_price, now_rate]],
-                                columns=['종목코드', '종목명', '시간', '가격', '등락률']), ignore_index=True)
-    print(stock_to_code[key], key, now, now_price, now_rate)
-    curs.execute(f"INSERT INTO stock VALUES(\"{stock_to_code[key]}\",\"{key}\",\"{now}\",\"{now_price}\",\"{now_rate}\")")
+    now_price = get_price(stock[1])
+    now_rate = get_rate(stock[1])
+
+    df = df.append(pd.DataFrame([[stock[1], stock[0], now, now_price, now_rate]],
+                                columns=['stockcode', 'stockname', 'time', 'price', 'rate']), ignore_index=True)
+    print([stock[1], stock[0], now, now_price, now_rate])
+    curs.execute(f"INSERT INTO stock VALUES(\"{stock[1]}\",\"{stock[0]}\",\"{now}\",\"{now_price}\",\"{now_rate}\")")
     conn.commit()
 
 print(df)
-
