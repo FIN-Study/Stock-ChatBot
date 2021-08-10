31dc3004853c267a38783f9aa3e6a60887578322
Yerin Kim <51941176+yerin1106@users.noreply.github.com>
2021-08-10 22:47:28 +0900

    21.08.10 Asynchronous programming

--- Stock/crawling.py
+++ Stock/crawling.py
@@ -3,7 +3,14 @@ import requests
 import pandas as pd
 from datetime import datetime
 import asyncio
-from functools import partial
+
+# 동기식 모델
+# 장점: 설계가 매우 간단하고 직관적
+# 단점: 요청에 따른 결과가 반환되기 전까지 아무것도 못하고 대기
+
+# 비동기식 모델
+# 장점: 요청에 따른 결과가 반환되는 시간 동안 다른 작업을 수행할 수 있음
+# 단점: 동기식보다 설계가 복잡하고 논증적
 
 # DB 연결
 import pymysql
@@ -19,7 +26,10 @@ curs = conn.cursor()
 async def get_price(company_code):
     url = 'https://finance.naver.com/item/main.nhn?code=' + company_code
 
+    # 현재의 이벤트 루프 가져오기
     loop = asyncio.get_event_loop()
+    # 지정된 실행기에서 requests.get이 호출되도록 배치
+    # async를 붙여 비동기 함수 선언, await로 비동기 작업을 대기
     req = await loop.run_in_executor(None, requests.get, url)
     html = req.text
     bs_obj = await loop.run_in_executor(None, BeautifulSoup,
@@ -62,7 +72,7 @@ async def main():
         sql_insert = f"INSERT INTO stock VALUES(\"{stock[1]}\",\"{stock[0]}\",\"{now}\",\"{now_price[0]}\",\"{now_rate[0]}\")"
         # 이후 실행시
         sql_update = f"UPDATE stock SET time=\"{now}\", price=\"{now_price[0]}\", rate=\"{now_rate[0]}\" WHERE stockcode=\"{stock[1]}\""
-        curs.execute(sql_insert)
+        curs.execute(sql_update)
         conn.commit()
 
     print(df)
@@ -81,6 +91,9 @@ if __name__ == "__main__":
     stock_to_code = tuple(zip(df_stock.회사명, df_stock.종목코드))
     # print(stock_to_code)
 
+    # 새 이벤트 루프를 만들어 현재 이벤트 루프로 설정
     loop = asyncio.get_event_loop()
+    # main()이 완료될 때까지 실행
     loop.run_until_complete(main())
+    # 이벤트 루프 닫기
     loop.close()
